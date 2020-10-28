eb03d8e2f694e2627040f85248e3afc36adc5a02
puddlejumper26 <xiang.wu@hotmail.de>
2020-10-28 11:02:35 +0100

    complete header and footer, and relate routing

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShareModule } from '../share/share.module';
+import { NgModule } from '@angular/core';
+
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ShareModule,
+    HomeModule,
+  ],
+  exports:[
+    HomeModule
   ]
 })
 export class PagesModule { }
