34e25f1fb0178faa43cc7e44227f98e427c3ae04
puddlejumper26 <xiang.wu@hotmail.de>
2021-01-21 22:55:39 +0100

    【Pop Window】 drag / drop (range) to pop window

--- src/app/share/wy-ui/wy-layer/wy-layer.module.ts
+++ src/app/share/wy-ui/wy-layer/wy-layer.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd';
 
@@ -7,7 +8,8 @@ import { WyLayerDefaultComponent } from './wy-layer-default/wy-layer-default.com
 
 @NgModule({
     declarations: [WyLayerModalComponent, WyLayerDefaultComponent],
-    imports: [CommonModule, NzButtonModule],
+    // angular cdk 的拖拽功能 https://material.angular.io/cdk/drag-drop/api
+    imports: [CommonModule, DragDropModule, NzButtonModule],
     exports: [WyLayerModalComponent, WyLayerDefaultComponent]
 })
 export class WyLayerModule {}
