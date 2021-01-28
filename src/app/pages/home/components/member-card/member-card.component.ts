b7b2ab07c0e1b27428e312db4ffce88e34de36c3
puddlejumper26 <xiang.wu@hotmail.de>
2021-01-28 20:58:51 +0100

    【WY Layer Login】control error through Interceptor-common.interceptor.ts

--- src/app/pages/home/components/member-card/member-card.component.ts
+++ src/app/pages/home/components/member-card/member-card.component.ts
@@ -1,3 +1,4 @@
+import { NzMessageService } from 'ng-zorro-antd';
 import { timer } from 'rxjs';
 import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 
@@ -17,13 +18,17 @@ export class MemberCardComponent implements OnInit {
     @Input() user: User;
     @Output() openModal = new EventEmitter<void>()
 
-    constructor(private memberServe: MemberService) {}
+    constructor(
+        private memberServe: MemberService,
+        private nzMessageServe: NzMessageService,
+        ) {}
 
     ngOnInit() {}
 
     onSignin() {
         this.memberServe.signin().subscribe( res => {
             // console.log('【MemberCardComponent】- onSignin - res - ', res);
+            this.alertMessage('success', '签到成功')
             this.tipTitle = "积分" + res.point;
             this.showTip = true;
             // 1.5秒后消失
@@ -32,7 +37,34 @@ export class MemberCardComponent implements OnInit {
                 this.tipTitle = '';
             });
         }, error =>{
-            console.log('【MemberCardComponent】- onSignin - error - ', error);
+            // console.log('【MemberCardComponent】- onSignin - error - ', error);
+            // 这里的功能可以用拦截器进行简化，因为目前如果希望能够直接显示 error中的 code 和 msg 信息（看下面的注释），需要 error.error
+            // 那么可以在拦截器中直接进行捕获错误
+            this.alertMessage('error', error.msg || '签到失败')
         });
     }
+
+    private alertMessage(type: string, msg: string) {
+        this.nzMessageServe.create(type, msg)
+    }
 }
+
+
+/**
+ *     直接 log  error 是这样的结果，
+ *
+ * HttpErrorResponse {
+ *     headers: HttpHeaders,
+ *     status: 400,
+ *     statusText: "Bad Request",
+ *     url: "http://localhost:3000/daily_signin?type=1",
+ *     ok: false, …}
+       error: {code: -2, msg: "重复签到"}
+       headers: HttpHeaders {normalizedNames: Map(0), lazyUpdate: null, lazyInit: ƒ}
+       message: "Http failure response for http://localhost:3000/daily_signin?type=1: 400 Bad Request"
+       name: "HttpErrorResponse"
+ *
+ *
+ *
+ *
+ */
--- src/app/services/http-interceptors/common.interceptor.ts
+++ src/app/services/http-interceptors/common.interceptor.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError } from 'rxjs/internal/operators/catchError';
 
 // 这个实际上是一个服务
 
@@ -14,6 +15,12 @@ export class CommonInterceptor implements HttpInterceptor {
             // 这里除了这样之外，可以在 头部添加任何的信息都可以
             //  现在每一个接口都会携带下面的属性
             withCredentials:true
-        }))
+        })).pipe(catchError(this.handleError));
+    }
+
+    // 这里就是拦截器的另外一个功能，统一处理错误的信息
+    // 这里的参数error就是member-card.component中 onSignin中的 error
+    private handleError(error: HttpErrorResponse): never {
+        throw error.error;
     }
 }
