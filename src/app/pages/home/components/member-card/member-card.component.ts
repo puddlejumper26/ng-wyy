fe818f678281791596f3d239b5d9f1f0ecc248f8
puddlejumper26 <xiang.wu@hotmail.de>
2021-01-28 20:39:49 +0100

    【WY Layer Login】added nz-tooltip to signin button

--- src/app/pages/home/components/member-card/member-card.component.ts
+++ src/app/pages/home/components/member-card/member-card.component.ts
@@ -1,3 +1,4 @@
+import { timer } from 'rxjs';
 import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 
 import { MemberService } from './../../../../services/member.service';
@@ -10,6 +11,9 @@ import { User } from "src/app/services/data-types/member.type";
 })
 export class MemberCardComponent implements OnInit {
 
+    tipTitle = '';
+    showTip = false;
+
     @Input() user: User;
     @Output() openModal = new EventEmitter<void>()
 
@@ -19,7 +23,14 @@ export class MemberCardComponent implements OnInit {
 
     onSignin() {
         this.memberServe.signin().subscribe( res => {
-            console.log('【MemberCardComponent】- onSignin - res - ', res);
+            // console.log('【MemberCardComponent】- onSignin - res - ', res);
+            this.tipTitle = "积分" + res.point;
+            this.showTip = true;
+            // 1.5秒后消失
+            timer(1500).subscribe(() => {
+                this.showTip = false;
+                this.tipTitle = '';
+            });
         }, error =>{
             console.log('【MemberCardComponent】- onSignin - error - ', error);
         });
