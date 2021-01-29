ab9852f3f92a6ef8a0f58d8e6b753cf8e7b1a36f
puddlejumper26 <xiang.wu@hotmail.de>
2021-01-29 19:17:00 +0100

    【Center】added center.html, applied app-single-sheet to here for display albums

--- src/app/pages/member/center/center.component.ts
+++ src/app/pages/member/center/center.component.ts
@@ -1,6 +1,8 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
 import { map } from 'rxjs/internal/operators/map';
+import { recordVal, User, UserSheet } from 'src/app/services/data-types/member.type';
 
 @Component({
     selector: "app-center",
@@ -8,15 +10,23 @@ import { map } from 'rxjs/internal/operators/map';
     styleUrls: ["./center.component.less"],
 })
 export class CenterComponent implements OnInit {
+
+    user: User;
+    userRecord: recordVal[];
+    userSheet: UserSheet;
+
     constructor(
         private route: ActivatedRoute
     ) {
         // console.log('【CenterComponent】- consctructor');
         // 这里的subscribe可看成解构赋值
         this.route.data.pipe(map(res => res.user)).subscribe(([ user, userRecord, userSheet]) => {
-            console.log('【CenterComponent】- consctructor - user - ', user);
-            console.log('【CenterComponent】- consctructor - userRecord - ', userRecord);
-            console.log('【CenterComponent】- consctructor - userSheet - ', userSheet);
+            // console.log('【CenterComponent】- consctructor - user - ', user);
+            // console.log('【CenterComponent】- consctructor - userRecord - ', userRecord);
+            // console.log('【CenterComponent】- consctructor - userSheet - ', userSheet);
+            this.user = user;
+            this.userRecord = userRecord;
+            this.userSheet = userSheet;
         })
     }
 
