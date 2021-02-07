b3e6e02f23863e0fae141e5c9a765a8d3195932c
puddlejumper26 <xiang.wu@hotmail.de>
2021-02-07 16:26:49 +0100

    【WY-Layer-Share】added validation for share text input(reactive form))

--- src/app/share/wy-ui/wy-layer/wy-layer-share/wy-layer-share.component.ts
+++ src/app/share/wy-ui/wy-layer/wy-layer-share/wy-layer-share.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, ChangeDetectionStrategy, Input } from "@angular/core";
 
 import { ShareInfo } from './../../../../store/reducers/member.reducer';
@@ -11,10 +12,29 @@ import { ShareInfo } from './../../../../store/reducers/member.reducer';
 export class WyLayerShareComponent implements OnInit {
 
     @Input() shareInfo: ShareInfo;
+    formModel: FormGroup
 
-    constructor() {
-        console.log('【WyLayerShareComponent】- constructor - this.shareInfo -', this.shareInfo)
+    constructor(
+        private fb: FormBuilder,
+    ) {
+        // console.log('【WyLayerShareComponent】- constructor - this.shareInfo -', this.shareInfo)
+        this.formModel = this.fb.group({
+            msg:['', Validators.maxLength(140)]
+        })
+
+        /**
+         *   上面的可以直接不同注入 FormBuilder 直接写
+         *
+         *      this.formModel = new FormGroup({
+         *          msg: new FormControl('', Validators.maxLength(140));
+         *      })
+         */
     }
 
     ngOnInit() {}
+
+    // 注意这里和模板结合
+    onSubmit() {
+
+    }
 }
