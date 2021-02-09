6a056ad7643228befae085fc63cd82cb6fda1f72
puddlejumper26 <xiang.wu@hotmail.de>
2021-02-09 15:46:49 +0100

    【WY-Layer-Register】using Array and ngFor for template in wy-code

--- src/app/share/wy-ui/wy-layer/wy-check-code/wy-code/wy-code.component.ts
+++ src/app/share/wy-ui/wy-layer/wy-check-code/wy-code/wy-code.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
+const CODELENGTH= 4;
+
 @Component({
     selector: "app-wy-code",
     templateUrl: "./wy-code.component.html",
@@ -7,7 +9,11 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WyCodeComponent implements OnInit {
-    constructor() {}
+    inputArr = [];
+    constructor() {
+        // 用空字符串填充一个长度为4 的数组
+        this.inputArr = Array(CODELENGTH).fill('');
+    }
 
     ngOnInit() {}
 }
